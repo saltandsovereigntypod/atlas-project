c1d27954bd16558e15fa229b8be3ba95fcc5668e
saltandsovereigntypod <saltandsovereigntybusiness@gmail.com>
2026-08-14 10:31:23 -0500

    Load Atlas revamp styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './editor.css'
+import './revamp.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
