60518187a217d76ff09d7b65960bfa66d6bd68bb
saltandsovereigntypod <saltandsovereigntybusiness@gmail.com>
2026-08-14 14:32:05 -0500

    Load nested navigation styles

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
 import './atlas.css'
+import './navigation.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>,
