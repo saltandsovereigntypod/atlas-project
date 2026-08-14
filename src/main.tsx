20e6e2a4bbe7aa50b35d506e91e44f640c6ef49a
saltandsovereigntypod <saltandsovereigntybusiness@gmail.com>
2026-08-14 14:19:23 -0500

    Load only the new Atlas interface styles

--- src/main.tsx
+++ src/main.tsx
@@ -3,9 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
 import './styles.css'
-import './editor.css'
-import './revamp.css'
-import './pages.css'
+import './atlas.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>,
