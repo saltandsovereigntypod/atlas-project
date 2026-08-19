c8eba3fc1bce73dc67cae88eea3e62d89e08badc
saltandsovereigntypod <saltandsovereigntybusiness@gmail.com>
2026-08-19 11:37:09 -0500

    Activate direct canvas text editing

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import { HashRouter } from 'react-router-dom'
 import App from './App'
+import { installDirectCanvasEditing } from './lib/directCanvasEditing'
 import './styles.css'
 import './atlas.css'
 import './navigation.css'
@@ -12,6 +13,8 @@ import './editor-sidebar-plus.css'
 import './editor-ux.css'
 import './widgets.css'
 import './tutorial.css'
+
+installDirectCanvasEditing()
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode><HashRouter><App /></HashRouter></React.StrictMode>,
